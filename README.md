9f1eb89e8bc3ff3d93b301b91436ec9fa5aa4ace
Devilson8 <DevilsonCosta8@gmail.com>
2024-08-01 14:40:28 +0200

    Riddle_First_Upload

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # API_Riddle
+
+In order to operate this server, you need to enter your port number, as it shows on the .env.example
---
+++ Server/index.js
@@ -0,0 +1,36 @@
+import {} from "dotenv/config";
+import express from "express";
+import axios from "axios";
+
+const PORT = process.env.PORT ?? 8000;
+// const API_KEY = process.env.API_KEY ?? null;
+// const API_URL = `https://riddles-api.vercel.app/random?APIKEY=${API_KEY}`;
+const API_URL = `https://riddles-api.vercel.app/random`;
+
+const app = express();
+
+app.use(express.static("public"));
+
+app.get("/", async (req, res) => {
+  try {
+    const response = await axios.get(API_URL);
+    const result = response.data;
+    res.render("index.ejs", { riddle: result.riddle, answer: result.answer });
+  } catch (error) {
+    res.render("index.ejs", { error: error.message });
+  }
+});
+
+app.post("/", async (req, res) => {
+  try {
+    const response = await axios.get(API_URL);
+    const result = response.data;
+    res.render("index.ejs", { riddle: result.riddle, answer: result.answer });
+  } catch (error) {
+    res.render("index.ejs", { error: error.message });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Your port ${PORT}, is now running`);
+});
